8488c1894d3e333bb3f326814b6600beed9f8b9f
Kim D Hendrick <kimdhendrick@gmail.com>
2026-07-27 15:35:10 -0600

    simplify routes

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,2 @@
 // Place any global data in this file.
 // Import it from anywhere in the site using the `import` keyword.
-
-export const SITE_TITLE = "Kim's Blog";
-export const SITE_DESCRIPTION = 'Hello, world!';
